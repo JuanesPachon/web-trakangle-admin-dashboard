6b3720fea35d2fdc468bf414f4d700a7f3f1d842
Sebastian <jsga2505@gmail.com>
2024-06-19 18:50:53 -0500

    edit register domain

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -9,4 +12,32 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+
+  registerForm = new FormGroup({
+    name: new FormControl("",{
+      validators: [ Validators.required, Validators.minLength(5) ],
+    }),
+    userName: new FormControl("",{
+      validators: [ Validators.required, Validators.minLength(5) ],
+    }),
+    email: new FormControl("",{
+      validators: [ Validators.required, Validators.minLength(5) ],
+    }),
+    password: new FormControl("",{
+      validators: [ Validators.required, Validators.minLength(5) ],
+    }),
+    
+  })
+
+  errorMessage: string = '';
+
+  
+
+  //show password
+
+  viewPassword = signal(false);
+
+  togglePassword() {
+    this.viewPassword.update(value => !value);
+  }
 }
