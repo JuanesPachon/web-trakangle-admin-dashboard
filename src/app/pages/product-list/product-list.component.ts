197d85288a720bb8c4869e133f92521cdf9f00cf
Juanes <juanespachon1203@gmail.com>
2024-06-19 18:53:41 -0500

    Add general info & create card component

--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
