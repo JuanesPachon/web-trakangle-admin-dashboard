5e3020d4d765fe15f491123ae7bd9bd551cba30d
Nelson <lvnelson.via@gmail.com>
2024-06-19 20:09:14 -0500

    add experience-list Domain

---
+++ src/app/pages/experience-list/experience-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './experience-list.component.html',
+  styleUrl: './experience-list.component.css'
+})
+export class ProductListComponent {
+
+}
+
