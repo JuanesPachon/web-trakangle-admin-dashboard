a75384e75961aedac6ce0ab0ada049b540100d2b
Juanes <juanespachon1203@gmail.com>
2024-06-19 21:28:11 -0500

    Add User list request

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private http = inject(HttpClient);
+
+  userList() {
+    return this.http.get('http://localhost:3000/users/');
+  }
+
 }
