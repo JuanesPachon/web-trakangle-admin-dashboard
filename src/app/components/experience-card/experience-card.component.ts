3615061e62c1ef2ecb80debf5abd2b816affde26
Nelson <lvnelson.via@gmail.com>
2024-06-21 18:59:32 -0500

    Add delete and edit request to experience list

--- src/app/components/experience-card/experience-card.component.ts
+++ src/app/components/experience-card/experience-card.component.ts
@@ -1,15 +1,89 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject, signal } from '@angular/core';
+import { ExperienceService } from '../../services/experience.service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './experience-card.component.html',
-  styleUrl: './experience-card.component.css'
+  styleUrl: './experience-card.component.css',
 })
 export class ExperienceCardComponent {
+  @Input() experience: any = {};
 
-  @Input() experience: any;
-  
+  private experienceService = inject(ExperienceService);
 
+  // Edit user Overlay logic
+
+  showEditExperienceOverlay = signal(false);
+  activeEditExperienceNotification = signal(false);
+
+  toggleEditExperienceOverlay() {
+    this.showEditExperienceOverlay.update((value) => !value);
+  }
+
+  // Create Request
+
+  errorMessage: string = '';
+
+  editForm = new FormGroup({
+    name: new FormControl(''),
+    place: new FormControl(''),
+    price: new FormControl(''),
+    description: new FormControl(''),
+  });
+
+  // Edit Request
+
+  onEditSubmit(event: Event) {
+    if (this.editForm.valid) {
+      this.experienceService
+        .editExperience(this.editForm.value, this.experience._id)
+        .subscribe({
+          next: (experience) => {
+            window.location.reload();
+            localStorage.setItem('showEditExperienceNotification', 'true');
+          },
+          error: (error) => {
+            if (error.status === 400) {
+              this.errorMessage = error.error.errors[0];
+            } else {
+              this.errorMessage =
+                'An unexpected error occurred. Please try again later.';
+            }
+            console.log(error);
+          },
+        });
+    }
+  }
+
+  // delete experience logic
+
+  deleteExperience() {
+    this.experienceService.deleteExperience(this.experience._id).subscribe({
+      next: () => {
+        window.location.reload();
+        localStorage.setItem('showDeleteExperienceNotification', 'true');
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  activeDeleteExperienceNotification = signal(false);
+
+  ngOnInit() {
+    if (localStorage.getItem('showEditExperienceNotification') === 'true') {
+      this.activeEditExperienceNotification.update((value) => !value);
+      localStorage.removeItem('showEditExperienceNotification');
+    }
+
+    if (localStorage.getItem('showDeleteExperienceNotification') === 'true') {
+      this.activeDeleteExperienceNotification.update((value) => !value);
+      localStorage.removeItem('showDeleteExperienceNotification');
+    }
+  }
 }
--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -31,4 +31,41 @@ export class ExperienceService {
     }, { headers: headers } )
 
   }
+  editExperience(formValues: any, experienceId: string) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('admin_token')}`,
+    });
+
+    const updateExperience: any = {};
+
+    if (formValues.name) {
+      updateExperience.name = formValues.name;
+    }
+    if (formValues.place) {
+      updateExperience.place = formValues.place;
+    }
+    if (formValues.price) {
+      updateExperience.price = formValues.price;
+    }
+    if (formValues.description) {
+      updateExperience.description = formValues.description;
+    }
+
+    return this.http.patch(`http://localhost:3000/experiences/${experienceId}`, updateExperience, {
+      headers: headers,
+    });
+  }
+
+  deleteExperience(experienceId: string) {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('admin_token')}`,
+    });
+
+    return this.http.delete(`http://localhost:3000/experiences/${experienceId}`, {
+      headers: headers,
+    });
+  }
+
 }
