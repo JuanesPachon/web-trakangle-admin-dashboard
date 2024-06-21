b34431c57bfabd0f80401c870992d63e481a023d
Nelson <lvnelson.via@gmail.com>
2024-06-20 20:38:33 -0500

    Add create experience request

--- src/app/pages/experience-list/experience-list.component.ts
+++ src/app/pages/experience-list/experience-list.component.ts
@@ -1,11 +1,14 @@
 import { Component, inject, signal } from '@angular/core';
 import { ExperienceService } from '../../services/experience.service';
 import { ExperienceCardComponent } from '../../components/experience-card/experience-card.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-experience-list',
   standalone: true,
-  imports: [ExperienceCardComponent],
+  imports: [ExperienceCardComponent, CommonModule, ReactiveFormsModule, RouterLinkWithHref],
   templateUrl: './experience-list.component.html',
   styleUrl: './experience-list.component.css'
 })
@@ -25,7 +28,62 @@ export class ExperienceListComponent{
         console.log(error);
       }
     });
+    
+    if (localStorage.getItem('showExperienceNotification') === 'true') {
+      this.activeNotification.update(value => !value);
+      localStorage.removeItem('showExperienceNotification');
+    }
 
   }
+
+    // Create user Overlay logic
+
+    showCreateOverlay = signal(false);
+    activeNotification = signal(false);
+  
+    toggleCreateOverlay() {
+      this.showCreateOverlay.update(value => !value);
+    }
+    errorMessage: string = '';
+
+    createExperienceForm = new FormGroup({
+      name: new FormControl("",{
+        validators: [ Validators.required, Validators.minLength(5) ],
+      }),
+      price: new FormControl("",{
+        validators: [ Validators.required, Validators.minLength(5) ],
+      }),
+      place: new FormControl("",{
+        validators: [ Validators.required, Validators.minLength(5) ],
+      }),
+      description: new FormControl("",{
+        validators: [ Validators.required, Validators.minLength(5) ],
+      }),
+      images: new FormControl("",{
+        validators: [ Validators.required, ],
+      }),
+      
+    })
+
+    onSubmit(event: Event){
+      if(this.createExperienceForm){
+        this.ExperienceService.createExperience(this.createExperienceForm.value).subscribe({
+          next: (Response)=>{
+            window.location.reload();
+            localStorage.setItem('showExperienceNotification', 'true');
+          },
+          error: (error)=>{
+            if (error.status === 400) {
+              this.errorMessage = error.error.errors[0];
+            } else {
+              this.errorMessage = 'An unexpected error occurred. Please try again later.';
+            }
+            console.log(error);
+          }
+        })
+      }
+
+    }
+  
 }
 
--- src/app/services/experience.service.ts
+++ src/app/services/experience.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,22 @@ export class ExperienceService {
   ExperienceList(page: number, limit?: number): Observable<any> {
     const queryParams = `?page=${page}${limit ? `&limit=${limit}` : ''}`;
     return this.http.get(`http://localhost:3000/experiences/${queryParams}`);
+  }
+
+  createExperience(formValues: any){
+    
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem('user_token')
+    });
+
+    return this.http.post("http://localhost:3000/experiences/", {
+      name: formValues.name,
+      place: formValues.place,
+      price: formValues.price,
+      description: formValues.description,
+      images: formValues.images
+    }, { headers: headers } )
+
   }
 }
